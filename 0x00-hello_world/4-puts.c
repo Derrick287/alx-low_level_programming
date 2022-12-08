409fcdb0416e6ce2993835802dd090499a7eecc6
Derrick287 <derrickwesonga287@gmail.com>
2022-12-08 00:50:38 -0800

    Displays a text using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   * Displays a text using puts
- *    * Return: Always 0 (Success)
- *     */
+ *  * main - is the entry point of the program
+ *   * Return: Always 0 shows success
+ *    */
 int main(void)
 {
-	    puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
