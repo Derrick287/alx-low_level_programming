98987931680c11a06f5021a6d682e7f6b7f9fdae
Derrick287 <derrickwesonga287@gmail.com>
2022-12-17 09:41:32 -0800

    Fabonacci

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	unsigned long febonacci1 = 0, febonacci2 = 1, sum;
+
+	for (i = 0; i < 50; ++i)
+	{
+		sum = febonacci1 + febonacci2;
+		printf("%lu", sum);
+
+		febonacci1 = febonacci2;
+		febonacci2 = sum;
+
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
