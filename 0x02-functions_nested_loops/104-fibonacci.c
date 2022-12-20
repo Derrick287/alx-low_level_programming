d2df79fc3f0b74fb53386aee622ef9aac35a65c4
Derrick287 <derrickwesonga287@gmail.com>
2022-12-19 22:53:20 -0800

    First 98 fabonacci starting with 1, 1

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-98 fibo numbers
+ * Return:0 is success
+ */
+int main(void)
+{
+        unsigned int pre = 1, cur = 2, temp, count, num = 98;
+        printf("%d, %d, ", pre, cur);
+        for(count = 2; count <= num; count++)
+        {
+                temp = cur + pre;
+                pre = cur;
+                cur = temp;
+                printf("%d, ", cur);
+        }
+
+        return 0;
+}
+
+
